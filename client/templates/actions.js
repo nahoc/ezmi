54cbc6a02c02e5a8b9b5470f44e703ec3300e2c7
cohanc <ccarpentier@effenti.ca>
2017-09-07 16:56:52 -0400

    Users can now select which board they want to put their tasks on

--- client/templates/actions.js
+++ client/templates/actions.js
@@ -5,28 +5,22 @@ import {
   ReactiveVar
 } from 'meteor/reactive-var';
 
-/*Template.dragList.onCreated(function dragListOnCreated() {
-  this.state = new ReactiveDict();
-  Meteor.subscribe('tasks');
-});*/
-
-
 Template.actions.events({
   'click #addTask, keypress input' (event) {
     if (event.which === 13 || event.type === 'click') {
       // Prevent default browser form submit
       event.preventDefault();
 
-
       // Get value from form element
       const target = event.target;
       const text = $('#taskInput').val();
       const isImportant = $("#isTaskImportant").is(":checked");
+      const parent = $("#selectTask").find(":selected").val();
 
       // Insert a task into the collection
       Tasks.insert({
         content: text,
-        parent: "backlog",
+        parent: parent,
         date: new Date(),
         isImportant: isImportant,
       });
